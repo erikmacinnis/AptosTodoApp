1dbedc6de88ba85096a0c67d3d57da5ae35353fe
erikmacinnis <erikhyun35@gmail.com>
2024-02-07 15:27:02 -0400

    feat: cleaned up create collection script

--- src/aptos/scripts/CreateCollection.ts
+++ src/aptos/scripts/CreateCollection.ts
@@ -1,91 +1,39 @@
-const fs = require('fs');
-const yaml = require('js-yaml');
-const path = require('path');
-const { Aptos, AptosConfig, Network, Account, Ed25519PrivateKey } = require("@aptos-labs/ts-sdk");
+require('dotenv').config();
+const { Aptos, AptosConfig, Network, Account, Ed25519PrivateKey, AccountAddress } = require("@aptos-labs/ts-sdk");
 
 const aptosConfig = new AptosConfig({ network: Network.TESTNET });
 const aptos = new Aptos(aptosConfig);
 
-const derivationPath = "m/44'/637'/0'/0'/0'"
-const mnemonicPhrase = 'drum car expand april six category group illness second garden wet acoustic'
+async function getAccount() {
+    const derivationPath = process.env.DERIVATION_PATH
+    const seedPhrase = process.env.MNEMONIC_PHRASE
 
-function getAccount2() {
-    // Get the current directory of the script
-    const currentDir = __dirname;
+    const privKey = await Ed25519PrivateKey.fromDerivationPath(derivationPath, seedPhrase)
+    const account = await Account.fromPrivateKey({privateKey: privKey})
 
-    // Build the full path to the config file
-    const filePath = path.join(currentDir, '../.aptos/config.yaml');
-
-    // Read the YAML file
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-
-    // Parse the YAML file into a JavaScript object
-    const config = yaml.load(fileContents);
-
-    console.log(config.profiles.default.private_key)
-    
-    // const privateKey = new PrivateKey(config.profiles.default.private_key)
-
-    // console.log(privateKey)
+    return account
 }
 
-async function generateAccount() { 
-    const alice = Account.generate();
+async function createCollection() {
+    const collectionName = process.env.COLLECTION_NAME
+    const description = process.env.COLLECTION_DESCRIPTION
+    const account = await getAccount();
 
-    await aptos.fundAccount({
-        accountAddress: alice.accountAddress,
-        amount: 100_000_000,
-      });
+    const createCollectionTransaction = await aptos.createCollectionTransaction({
+        creator: account,
+        description: description,
+        name: collectionName,
+        uri: '',
+    })
 
-    return alice
+    const createCollectionTx = await aptos.signAndSubmitTransaction({
+        signer: account,
+        transaction: createCollectionTransaction,
+    });
+
+    await aptos.waitForTransaction({transactionHash:createCollectionTx.hash});
+
+    console.log(createCollectionTx)
 }
 
-async function main() {
-    console.log('here')
-    const privateKey = Ed25519PrivateKey.fromDerivationPath(derivationPath, mnemonicPhrase)
-    console.log(privateKey);
-    const alice = await Account.fromPrivateKey({privateKey})
-    console.log(alice.accountAddress.toString());
-    // const alice = await generateAccount();
-    // const bob = await generateAccount()
-
-    // console.log(alice.accountAddress.toString())
-
-    // const collectionName = 'todo-aptos'
-
-    // const createCollectionTransaction = await aptos.createCollectionTransaction({
-    //     creator: alice,
-    //     description: 'nfts for a todo app',
-    //     name: collectionName,
-    //     uri: '',
-    // })
-
-    // const createCollectionTx = await aptos.signAndSubmitTransaction({
-    //     signer: alice,
-    //     transaction: createCollectionTransaction,
-    // });
-
-    // await aptos.waitForTransaction({transactionHash:createCollectionTx.hash});
-
-    // console.log(createCollectionTx)
-
-    // const mintTokenTransaction = await aptos.mintDigitalAssetTransaction({
-    //     creator: alice,
-    //     collection: collectionName,
-    //     description: 'token for nft for todo app',
-    //     name: 'CompletedTodo',
-    //     uri: '',
-    // });
-
-    // const mintTokenTx =  await aptos.signAndSubmitTransaction({
-    //     signer: alice,
-    //     transaction: mintTokenTransaction,
-    // });
-
-    // await aptos.waitForTransaction({transactionHash:mintTokenTx.hash});
-
-    // console.log(mintTokenTx)
-}
-
-main()
-
+createCollection()
